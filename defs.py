fe299e7ba575a9fb315e8634f1d2453c71dec440
Phillip <phillip.besse@gmail.com>
2022-12-10 13:01:10 -0500

    complete draw

--- defs.py
+++ defs.py
@@ -44,20 +44,26 @@ class Deck:
     # Shuffles the deck of cards. This means randomzing the order of the cards in the Deck.
     def shuffle(self) -> None:
         random.shuffle(self.deck)
-        
+
     # Returns the top Card in the deck, but does not modify the deck.
     def peek(self) -> Card:
         pass
+
     # Removes and returns the top card in the deck. The card should no longer be in the Deck.
     def draw(self) -> Card:
-        pass
+        draw_card = self.deck.pop()
+        # or self.deck.remove(0)?
+        return draw_card
+
     # Adds the input card to the deck. 
     # If the deck has more than 52 cards, do not add the card and raise an exception.
     def add_card(self, card: Card) -> None:
         pass
+
     # Calling this function should print all the cards in the deck in their current order.
     def print_deck(self) -> None:
         pass
+
     # Resets the deck to it's original state with all 52 cards.
     # Also shuffle the deck.
     def reset(self) -> None:
