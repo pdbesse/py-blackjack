69d40136aa2026adb921b71c032b2ddad543d02b
Phillip <phillip.besse@gmail.com>
2022-12-12 14:18:40 -0500

    add blackjack __init__

--- defs.py
+++ defs.py
@@ -13,11 +13,11 @@ class Card:
 
     # Returns the suit of the card.
     def suit(self) -> str:
-        return self.suit
+        print(self.suit)
 
     # Returns the value of the card.
     def value(self) -> str:
-        return self.value
+        print(self.value)
 
     # Returns a string representation of Card
     # E.g. "Ace of Spades"
@@ -37,6 +37,8 @@ class Deck:
       for suit in SUITS:
         for value in VALUES:
             self.deck.append(Card(suit, value))
+            
+        self.shuffle()
     
     # Returns the number of Cards in the Deck
     def size(self) -> int:
@@ -53,8 +55,8 @@ class Deck:
     # Removes and returns the top card in the deck. The card should no longer be in the Deck.
     def draw(self) -> Card:
         # draw_card = self.deck.pop()
-        draw_card = self.deck.remove(0)
-        print(draw_card)
+        drawn_card = self.deck.remove(0)
+        return drawn_card
 
     # Adds the input card to the deck. 
     # If the deck has more than 52 cards, do not add the card and raise an exception.
@@ -63,9 +65,59 @@ class Deck:
 
     # Calling this function should print all the cards in the deck in their current order.
     def print_deck(self) -> None:
-        print(self.deck)
+        for i in range(len(self.deck)):
+            print(self.deck[i])
 
     # Resets the deck to it's original state with all 52 cards.
     # Also shuffle the deck.
     def reset(self) -> None:
+        self.__init__()
+        self.shuffle()
+
+class Blackjack:
+  # Creates a Blackjack game with a new Deck.
+  def __init__(self):
+        deck = Deck.__init__()  
+        # init empty hand
+        hand = []
+        # draw two cards hand
+        hand.append(Deck.draw())
+        hand.append(Deck.draw())
+
+  # Computes the score of a hand. 
+  # For examples of hands and scores as a number.
+  # 2,5 -> 7
+  # 3, 10 -> 13
+  # 5, King -> 15
+  # 10, Ace -> 21
+  # 10, 8, 4 -> Bust so return -1
+  # 9, Jack, Ace -> 20 
+  # If the Hand is a bust return -1 (because it always loses)
+  def _get_score(self, hand: List[Card]) -> int:
+
+        pass
+  
+  # Prints the current hand and score.
+  # E.g. would print out (Ace of Clubs, Jack of Spades, 21)
+  # E.g. (Jack of Clubs, 5 of Diamonds, 8 of Hearts, "Bust!")
+  def _print_current_hand(self) -> None:
+        pass
+  
+  # The previous hand is discarded and shuffled back into the deck.
+  # Should remove the top 2 cards from the current deck and 
+  # Set those 2 cards as the "current hand". 
+  # It should also print the current hand and score of that hand.
+  # If less than 2 cards are in the deck, 
+  # then print an error instructing the client to shuffle the deck.
+  def deal_new_hand(self) -> None:
+        pass
+
+  # Deals one more card to the current hand and prints the hand and score.
+  # If no cards remain in the deck, print an error.
+  def hit(self) -> None: 
+        pass
+  
+  # Reshuffles all cards in the "current hand" and "discard pile"
+  # and shuffles everything back into the Deck.
+  def reshuffle(self) -> None:
         pass
